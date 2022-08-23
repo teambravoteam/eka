6b060ce24955fc08f4c31b2881913f2533ae625c
LeeYeongJae2 <103983420+LeeYeongJae2@users.noreply.github.com>
2022-08-23 09:50:14 +0900

    hi yeong

---
+++ eka/src/main/java/com/varxyz/eka/student/domain/FindStudent.java
@@ -0,0 +1,23 @@
+package com.varxyz.eka.student.domain;
+
+
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Getter
+@Setter
+@ToString
+public class FindStudent {
+	
+	private String name;
+	private String ekacheck;
+	private String gendercheck;
+	private String school;
+	private String grade;
+	
+	
+}
